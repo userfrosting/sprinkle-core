77a5575ac5a18df5ef5d14825e599d1bf4d21ee1
Louis Charette <charette.louis@gmail.com>
2021-06-26 16:37:18 -0400

    Update AlertStreamServiceTest

--- src/ServicesProvider/AlertStreamService.php
+++ src/ServicesProvider/AlertStreamService.php
@@ -32,13 +32,19 @@ class AlertStreamService implements ServicesProviderInterface
     public function register(): array
     {
         return [
+            // TODO : Custom exception should be used.
+            // TODO : Container could be used to instantiate *AlertStream and limit the number of dependencies required (but would depend on the whole container... PHP-DI docs should be consulted to find the best way to do this).
             AlertStream::class => function (Config $config, Translator $translator, Cache $cache, Session $session) {
-                if ($config['alert.storage'] == 'cache') {
-                    return new CacheAlertStream($config['alert.key'], $translator, $cache, $session->getId());
-                } elseif ($config['alert.storage'] == 'session') {
-                    return new SessionAlertStream($config['alert.key'], $translator, $session);
-                } else {
-                    throw new \Exception("Bad alert storage handler type '{$config['alert.storage']}' specified in configuration file.");
+                switch ($config->get('alert.storage')) {
+                    case 'cache':
+                        return new CacheAlertStream($config->get('alert.key'), $translator, $cache, $session->getId());
+                    break;
+                    case 'session':
+                        return new SessionAlertStream($config->get('alert.key'), $translator, $session);
+                    break;
+                    default:
+                        throw new \Exception("Bad alert storage handler type '{$config->get('alert.storage')}' specified in configuration file.");
+                    break;
                 }
             },
         ];
---
+++ tests/Integration/ServicesProvider/AlertStreamServiceTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @copyright Copyright (c) 2019 Alexander Weissman
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/LICENSE.md (MIT License)
+ */
+
+namespace UserFrosting\Sprinkle\Core\Tests\Integration\ServicesProvider;
+
+use DI\Container;
+use Illuminate\Cache\Repository as Cache;
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use UserFrosting\Alert\AlertStream;
+use UserFrosting\Alert\CacheAlertStream;
+use UserFrosting\Alert\SessionAlertStream;
+use UserFrosting\Sprinkle\Core\ServicesProvider\AlertStreamService;
+use UserFrosting\Testing\ContainerStub;
+use UserFrosting\Support\Repository\Repository as Config;
+use UserFrosting\I18n\Translator;
+use UserFrosting\Session\Session;
+
+/**
+ * Integration tests for `alerts` service.
+ * Check to see if service returns what it's supposed to return
+ */
+class AlertStreamServiceTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    protected Container $ci;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Create container with provider to test
+        $provider = new AlertStreamService();
+        $this->ci = ContainerStub::create($provider->register());
+    }
+
+    public function testCacheConfig(): void
+    {
+        // Set dependencies services
+        $config = m::mock(Config::class);
+        $config->shouldReceive('get')->with('alert.storage')->andReturn('cache');
+        $config->shouldReceive('get')->with('alert.key')->andReturn('foo');
+        $this->ci->set(Config::class, $config);
+        $this->ci->set(Cache::class, m::mock(Cache::class));
+        $this->ci->set(Translator::class, m::mock(Translator::class));
+        $session = m::mock(Session::class);
+        $session->shouldReceive('getId')->andReturn('foobar');
+        $this->ci->set(Session::class, $session);
+
+        // Get stream and assert the right one is returned based on config
+        $this->assertInstanceOf(CacheAlertStream::class, $this->ci->get(AlertStream::class));
+    }
+
+    public function testSessionConfig(): void
+    {
+        // Set dependencies services
+        $config = m::mock(Config::class);
+        $config->shouldReceive('get')->with('alert.storage')->andReturn('session');
+        $config->shouldReceive('get')->with('alert.key')->andReturn('foo');
+        $this->ci->set(Config::class, $config);
+        $this->ci->set(Cache::class, m::mock(Cache::class));
+        $this->ci->set(Translator::class, m::mock(Translator::class));
+        $this->ci->set(Session::class, m::mock(Session::class));
+
+        // Get stream and assert the right one is returned based on config
+        $this->assertInstanceOf(SessionAlertStream::class, $this->ci->get(AlertStream::class));
+    }
+
+    public function testBadConfig(): void
+    {
+        // Set dependencies services
+        $config = m::mock(Config::class);
+        $config->shouldReceive('get')->with('alert.storage')->andReturn('foo');
+        $this->ci->set(Config::class, $config);
+        $this->ci->set(Cache::class, m::mock(Cache::class));
+        $this->ci->set(Translator::class, m::mock(Translator::class));
+        $this->ci->set(Session::class, m::mock(Session::class));
+
+        // Get stream and assert the exception is thrown.
+        $this->expectException(\Exception::class);
+        $this->ci->get(AlertStream::class);
+    }
+}
