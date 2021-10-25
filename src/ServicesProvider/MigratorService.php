fdf9db61a646f8a1e1642a0ae620d86fb17acd02
Louis Charette <charette.louis@gmail.com>
2021-10-25 19:43:54 -0400

    Update MigratorService

--- src/ServicesProvider/MigratorService.php
+++ src/ServicesProvider/MigratorService.php
@@ -15,41 +15,21 @@ use UserFrosting\ServicesProvider\ServicesProviderInterface;
 use UserFrosting\Sprinkle\Core\Database\Migrator\DatabaseMigrationRepository;
 use UserFrosting\Sprinkle\Core\Database\Migrator\MigrationLocatorInterface;
 use UserFrosting\Sprinkle\Core\Database\Migrator\MigrationRepositoryInterface;
-use UserFrosting\Sprinkle\Core\Database\Migrator\Migrator;
 use UserFrosting\Sprinkle\Core\Database\Migrator\SprinkleMigrationLocator;
 use UserFrosting\Support\Repository\Repository as Config;
 
 /*
  * Migrator service.
  *
- * This service handles database migration operations
- *
- * @return \UserFrosting\Sprinkle\Core\Database\Migrator\Migrator
+ * This service handles database migration operations.
 */
 class MigratorService implements ServicesProviderInterface
 {
     public function register(): array
     {
         return [
-            // Migrator::class => function (
-            //     Capsule $db,
-            //     MigrationRepositoryInterface $migrationRepository,
-            //     MigrationLocatorInterface $migrationLocator
-            // ) {
-            //     $migrator = new Migrator(
-            //         $db,
-            //         $migrationRepository,
-            //         $migrationLocator,
-            //     );
-
-            //     return $migrator;
-            // },
-
-            // TODO : Should probably depend on a service, not a string ?
             MigrationRepositoryInterface::class => function (Capsule $db, Config $config) {
-                $repository = new DatabaseMigrationRepository($db, $config->get('migrations.repository_table'));
-
-                return $repository;
+                return new DatabaseMigrationRepository($db, $config->get('migrations.repository_table'));
             },
 
             MigrationLocatorInterface::class => \DI\autowire(SprinkleMigrationLocator::class),
