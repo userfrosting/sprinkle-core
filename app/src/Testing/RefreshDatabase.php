9278126f22a126e69e85c33e412f53d2d13b61a3
Louis Charette <charette.louis@gmail.com>
2022-01-21 22:09:16 -0500

    Fix RefreshDatabase

--- app/src/Testing/RefreshDatabase.php
+++ app/src/Testing/RefreshDatabase.php
@@ -59,7 +59,5 @@ trait RefreshDatabase
         // Refresh the Database. Rollback all migrations and start over
         $this->ci->get(Migrator::class)->reset();
         $this->ci->get(Migrator::class)->migrate();
-
-        self::$migrated = true;
     }
 }
