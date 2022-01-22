b76a2161f78e2290bf0a3c5ed31b443b15db6054
Louis Charette <charette.louis@gmail.com>
2022-01-21 21:37:04 -0500

    Update RefreshDatabase

--- app/src/Testing/RefreshDatabase.php
+++ app/src/Testing/RefreshDatabase.php
@@ -11,21 +11,14 @@
 namespace UserFrosting\Sprinkle\Core\Testing;
 
 use DI\Container;
-use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Database\Connection;
 use UserFrosting\Sprinkle\Core\Database\Migrator\Migrator;
 
 /**
- * Trait enabling wrapping of each test case in a database transaction
- * Based on Laravel `RefreshDatabase` Traits.
+ * Trait o refresh database to fresh state.
  */
 trait RefreshDatabase
 {
-    /**
-     * @var bool Indicates if the test database has been migrated.
-     */
-    public static bool $migrated = false;
-
     /**
      * Define hooks to migrate the database before and after each test.
      */
@@ -53,7 +46,7 @@ trait RefreshDatabase
     /**
      * Refresh the in-memory database.
      */
-    protected function refreshInMemoryDatabase(): void
+    private function refreshInMemoryDatabase(): void
     {
         $this->ci->get(Migrator::class)->migrate();
     }
@@ -61,47 +54,12 @@ trait RefreshDatabase
     /**
      * Refresh a conventional test database.
      */
-    protected function refreshTestDatabase(): void
+    private function refreshTestDatabase(): void
     {
-        if (!self::$migrated) {
+        // Refresh the Database. Rollback all migrations and start over
+        $this->ci->get(Migrator::class)->reset();
+        $this->ci->get(Migrator::class)->migrate();
 
-            // Refresh the Database. Rollback all migrations and start over
-            $this->ci->get(Migrator::class)->reset();
-            $this->ci->get(Migrator::class)->migrate();
-
-            self::$migrated = true;
-        }
-
-        // $this->beginDatabaseTransaction();
-    }
-
-    /**
-     * Handle database transactions on the specified connections.
-     */
-    protected function beginDatabaseTransaction(): void
-    {
-        $database = $this->ci->get(Capsule::class);
-
-        foreach ($this->connectionsToTransact() as $name) {
-            $database->connection($name)->beginTransaction();
-        }
-
-        // TODO : Not used anymore !
-        $this->beforeApplicationDestroyed(function () use ($database) {
-            foreach ($this->connectionsToTransact() as $name) {
-                $database->connection($name)->rollBack();
-            }
-        });
-    }
-
-    /**
-     * The database connections that should have transactions.
-     *
-     * @return array
-     */
-    protected function connectionsToTransact(): array
-    {
-        return property_exists($this, 'connectionsToTransact')
-                            ? $this->connectionsToTransact : [null];
+        self::$migrated = true;
     }
 }
