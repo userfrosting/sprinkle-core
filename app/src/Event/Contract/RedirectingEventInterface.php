af8230146e2dcaca06545ef4d57822467b4349f7
Louis Charette <charette.louis@gmail.com>
2022-03-27 20:32:24 -0400

    Add interface for RedirectingEvent + Helper traits

---
+++ app/src/Event/Contract/RedirectingEventInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * UserFrosting Core Sprinkle (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/sprinkle-core
+ * @copyright Copyright (c) 2021 Alexander Weissman & Louis Charette
+ * @license   https://github.com/userfrosting/sprinkle-core/blob/master/LICENSE.md (MIT License)
+ */
+
+namespace UserFrosting\Sprinkle\Core\Event\Contract;
+
+/**
+ * Event that can redirect the user to another page.
+ */
+interface RedirectingEventInterface
+{
+    /**
+     * Get the value of redirect.
+     *
+     * @return string
+     */
+    public function getRedirect(): ?string;
+}
---
+++ app/tests/Unit/Event/RedirectTraitTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * UserFrosting Core Sprinkle (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/sprinkle-core
+ * @copyright Copyright (c) 2021 Alexander Weissman & Louis Charette
+ * @license   https://github.com/userfrosting/sprinkle-core/blob/master/LICENSE.md (MIT License)
+ */
+
+namespace UserFrosting\Sprinkle\Core\Tests\Integration\ServicesProvider;
+
+use PHPUnit\Framework\TestCase;
+use UserFrosting\Sprinkle\Core\Error\ShutdownHandler;
+use UserFrosting\Sprinkle\Core\Event\Contract\RedirectingEventInterface;
+use UserFrosting\Sprinkle\Core\Event\Helper\RedirectTrait;
+
+/**
+ * Test RedirectTrait
+ */
+class RedirectTraitTest extends TestCase
+{
+    public function testTrait(): void
+    {
+        $event = new RedirectedEvent();
+
+        $this->assertNull($event->getRedirect());
+        $event->setRedirect('/home');
+        $this->assertSame('/home', $event->getRedirect());
+    }
+}
+
+class RedirectedEvent implements RedirectingEventInterface
+{
+    use RedirectTrait;
+}
