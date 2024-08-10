9fb9c685335e61373263adb1f2a003e64149dac1
Louis Charette <charette.louis@gmail.com>
2024-08-09 20:57:48 -0400

    Add getter function

--- app/assets/stores/config.ts
+++ app/assets/stores/config.ts
@@ -1,11 +1,17 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
+import { getProperty } from 'dot-prop'
 
 export const useConfigStore = defineStore('config', {
     persist: true,
     state: () => {
         return {
             config: {}
+        }
+    },
+    getters: {
+        get: (state) => {
+            return (key: string, value?: any): any => getProperty(state.config, key, value)
         }
     },
     actions: {
