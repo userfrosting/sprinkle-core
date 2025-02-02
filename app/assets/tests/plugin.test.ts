4cad5982184b7a41028099816d034e71575e23ff
Louis Charette <charette.louis@gmail.com>
2025-02-02 10:16:52 -0500

    Fix test

--- app/assets/tests/plugin.test.ts
+++ app/assets/tests/plugin.test.ts
@@ -2,14 +2,20 @@ import { describe, expect, test, vi } from 'vitest'
 import { useConfigStore } from '../stores/config'
 import plugin from '..'
 import * as Config from '../stores/config'
+import * as Translator from '../stores/useTranslator'
 
 const mockConfigStore = {
+    load: vi.fn()
+}
+
+const mockTranslatorStore = {
     load: vi.fn()
 }
 
 describe('Plugin', () => {
     test('should install the plugin and initiate load', () => {
         vi.spyOn(Config, 'useConfigStore').mockReturnValue(mockConfigStore as any)
+        vi.spyOn(Translator, 'useTranslator').mockReturnValue(mockTranslatorStore as any)
 
         plugin.install()
 
