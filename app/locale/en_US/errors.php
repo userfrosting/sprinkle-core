e4f76fbcedbcfed9bc011e2cf45bc857016347b5
Louis Charette <charette.louis@gmail.com>
2025-03-01 16:08:50 -0500

    Fix style

--- app/locale/en_US/errors.php
+++ app/locale/en_US/errors.php
@@ -47,7 +47,7 @@ return [
         'DESCRIPTION' => "Oops, looks like our server might have goofed. If you're an admin, please check the PHP or UserFrosting logs.",
 
         'MAIL' => 'Fatal error attempting mail, contact your server administrator.  If you are the admin, please check the UserFrosting log.',
-        'MISC' => 'An error occurred.',//OK
+        'MISC' => 'An error occurred.',
 
         'RATE_LIMIT_EXCEEDED' => [
             'TITLE'       => 'Rate Limit Exceeded',
