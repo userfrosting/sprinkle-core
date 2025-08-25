61b8946b2bcaecd34c1521b1a3eebcb5dc3a8202
Louis Charette <charette.louis@gmail.com>
2025-08-24 22:03:23 -0400

    Update support for Slim 4.15

--- app/src/Util/RouteParserInterface.php
+++ app/src/Util/RouteParserInterface.php
@@ -17,15 +17,18 @@ use Psr\Http\Message\UriInterface;
 use RuntimeException;
 use Slim\Interfaces\RouteParserInterface as SlimRouteParserInterface;
 
+/**
+ * Add fallback route to Slim's RouteParserInterface.
+ */
 interface RouteParserInterface extends SlimRouteParserInterface
 {
     /**
      * Build the path for a named route excluding the base path.
      *
-     * @param string                $routeName     Route name
-     * @param array<string, string> $data          Named argument replacement data
-     * @param array<string, string> $queryParams   Optional query string parameters
-     * @param string|null           $fallbackRoute Optional fallback route (the actual route, not the route name)
+     * @param string                                           $routeName     Route name
+     * @param array<string, string>                            $data          Named argument replacement data
+     * @param array<string, string | array<array-key, string>> $queryParams   Optional query string parameters
+     * @param string|null                                      $fallbackRoute Optional fallback route (the actual route, not the route name)
      *
      * @throws RuntimeException         If named route does not exist
      * @throws InvalidArgumentException If required data not provided
@@ -40,10 +43,10 @@ interface RouteParserInterface extends SlimRouteParserInterface
     /**
      * Build the path for a named route including the base path.
      *
-     * @param string                $routeName     Route name
-     * @param array<string, string> $data          Named argument replacement data
-     * @param array<string, string> $queryParams   Optional query string parameters
-     * @param string|null           $fallbackRoute Optional fallback route (the actual route, not the route name)
+     * @param string                                           $routeName     Route name
+     * @param array<string, string>                            $data          Named argument replacement data
+     * @param array<string, string | array<array-key, string>> $queryParams   Optional query string parameters
+     * @param string|null                                      $fallbackRoute Optional fallback route (the actual route, not the route name)
      *
      * @throws RuntimeException         If named route does not exist
      * @throws InvalidArgumentException If required data not provided
@@ -58,11 +61,11 @@ interface RouteParserInterface extends SlimRouteParserInterface
     /**
      * Get fully qualified URL for named route.
      *
-     * @param UriInterface          $uri
-     * @param string                $routeName     Route name
-     * @param array<string, string> $data          Named argument replacement data
-     * @param array<string, string> $queryParams   Optional query string parameters
-     * @param string|null           $fallbackRoute Optional fallback route (the actual route, not the route name)
+     * @param UriInterface                                     $uri
+     * @param string                                           $routeName     Route name
+     * @param array<string, string>                            $data          Named argument replacement data
+     * @param array<string, string | array<array-key, string>> $queryParams   Optional query string parameters
+     * @param string|null                                      $fallbackRoute Optional fallback route (the actual route, not the route name)
      */
     public function fullUrlFor(
         UriInterface $uri,
