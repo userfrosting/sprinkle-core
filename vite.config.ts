5d4aa41582a6ee169c13d93d09706907f6363b5a
Louis Charette <charette.louis@gmail.com>
2025-09-06 14:48:47 -0400

    Cleanup package(s).json

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 // https://stackoverflow.com/a/74397545/445757
 export default defineConfig({
-    plugins: [vue(), dts()],
-    publicDir: false,
-    build: {
-        outDir: './dist',
-        lib: {
-            entry: {
-                main: 'app/assets/index.ts',
-                interfaces: 'app/assets/interfaces/index.ts',
-                stores: 'app/assets/stores/index.ts',
-                composables: 'app/assets/composables/index.ts'
-            }
-        },
-        rollupOptions: {
-            external: ['vue', 'pinia'],
-            output: {
-                exports: 'named',
-                globals: {
-                    vue: 'Vue',
-                }
-            }
-        }
-    },
+    plugins: [vue()],
     test: {
         coverage: {
             reportsDirectory: './_meta/_coverage',
