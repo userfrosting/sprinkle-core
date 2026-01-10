63379debaac19860ec2bb5b814e1af14050204d1
Louis Charette <charette.louis@gmail.com>
2026-01-10 16:19:14 -0500

    Correctly externalize vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
         rollupOptions: {
             external: [
                 'vue',
+                'vue-router',
                 'axios',
                 'pinia',
                 'pinia-plugin-persistedstate',
