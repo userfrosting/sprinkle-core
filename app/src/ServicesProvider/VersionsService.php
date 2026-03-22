1a411659977629c6bee7f380b97d9be581a448f4
Louis Charette <charette.louis@gmail.com>
2026-03-22 18:53:08 -0400

    Change PHP_RECOMMENDED_VERSION

--- app/src/ServicesProvider/VersionsService.php
+++ app/src/ServicesProvider/VersionsService.php
@@ -29,7 +29,7 @@ class VersionsService implements ServicesProviderInterface
         return [
             // Minimum requirements
             'PHP_MIN_VERSION'              => '^8.0',
-            'PHP_RECOMMENDED_VERSION'      => '^8.2',
+            'PHP_RECOMMENDED_VERSION'      => '^8.5',
             'NODE_MIN_VERSION'             => '>=18',
             'NPM_MIN_VERSION'              => '>=9',
 
